a699e8a28705afb86e93c5e0e2eaf1dccb9870d5
Macharla-Nithyusha <125576114+Macharla-Nithyusha@users.noreply.github.com>
2025-10-20 14:40:14 +0530

    Update emailModel.ts

--- backend/src/ models/emailModel.ts
+++ backend/src/ models/emailModel.ts
@@ -1,1 +1,12 @@
+export interface Email {
+  id: string;
+  subject: string;
+  from: string;
+  to: string[];
+  date: string;
+  body: string;
+  folder: string;
+  account: string;
+  category?: string;
+}
 
