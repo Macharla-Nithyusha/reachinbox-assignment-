6e124a0c0fdcc7a15eda183c8dd8e60fc8855cb1
Macharla-Nithyusha <125576114+Macharla-Nithyusha@users.noreply.github.com>
2025-10-20 14:39:24 +0530

    Add IMAP accounts configuration

--- backend/src/config/imapConfig.ts
+++ backend/src/config/imapConfig.ts
@@ -1,1 +1,11 @@
+export const IMAP_ACCOUNTS = [
+  {
+    user: process.env.IMAP_USER_1!,
+    pass: process.env.IMAP_PASS_1!,
+  },
+  {
+    user: process.env.IMAP_USER_2!,
+    pass: process.env.IMAP_PASS_2!,
+  },
+];
 
