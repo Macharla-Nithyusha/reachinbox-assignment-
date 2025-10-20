b5ef9d449fccf3299266a1b47305fde845e4737d
Macharla-Nithyusha <125576114+Macharla-Nithyusha@users.noreply.github.com>
2025-10-20 14:47:21 +0530

    Add getEmails function to fetch emails from API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,1 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+export const getEmails = async () => {
+  const res = await axios.get(`${BASE_URL}/emails`);
+  return res.data;
+};
 
