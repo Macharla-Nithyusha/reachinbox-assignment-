0ac8d107f893776204f9af126a8db51a4f5225d0
Macharla-Nithyusha <125576114+Macharla-Nithyusha@users.noreply.github.com>
2025-10-20 14:39:02 +0530

    Update elasticsearch.ts

--- backend/src/config/elasticsearch.ts
+++ backend/src/config/elasticsearch.ts
@@ -1,1 +1,6 @@
+import { Client } from "@elastic/elasticsearch";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const esClient = new Client({ node: process.env.ES_URL || "http://localhost:9200" });
 
